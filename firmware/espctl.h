099cec6efbb5921da106649ba0ec1120c8d65ec3
Anirudh Sahu <renudilip19@gmail.com>
2026-07-14 20:12:54 +0530

    added more structure and slider

--- firmware/espctl.h
+++ firmware/espctl.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-#define ESPCTL_MAx_WIDGET 16
+#define ESPCTL_MAX_WIDGET 16
 #define ESPCTL_MAX_PINS 16
 
 #define ESPCTL_BUF 128
@@ -22,6 +22,25 @@ class ESPCtl {
         Serial.begin(baud);
         _lineLen = 0;
     }
+
+
+
+    void addSlider(String id, int min, int max, int defVal){
+        if ( _widgetCount >= ESPCTL_MAX_WIDGET) return;
+        Widget &w = _widgets[_widgetCount++];
+        w.id = id;
+        w.type = WIDGET_SLIDER;
+        w.min = min;
+        w.max = max;
+        w.value = defVal;
+    }
+    int getSlider(String id){
+        Widget *w = findWidget(id);
+        return w ? (w->value != 0) : false;
+    }
+
+
+
     void update(){
         while (Serial.available()){
             char c = Serial.read();
@@ -33,5 +52,44 @@ class ESPCtl {
                 _line[_lineLen++] = c;
             }
         }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    private:
+    enum WidgetType {
+        WIDGET_SLIDER,
+    };
+
+    struct Widget {
+        String id;
+        WidgetType type;
+        int min;
+        int max;
+        int value;
+    }
+
+
+    Widget _widgets[ESPCTL_MAX_WIDGET];
+    int _widgetCount = 0;
+    Pin _pins[ESPCTL_MAX_PINS];
+    int _pinCount = 0;
+    char _line[ESPCTL_BUF];
+    int _lineLen = 0;
+
+
+    Widget *findWidget(const String &id){
+        for (int i = 0; i < _widgetCount; i++){
+            if (_widget[i].id == id) return &_widgets[i];
+        }
+        return nullptr;
     }
 }
