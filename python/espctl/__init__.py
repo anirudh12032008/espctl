db6920e3f2433c4e1951be4a314e5a4ce5485214
Anirudh Sahu <renudilip19@gmail.com>
2026-07-15 19:45:09 +0530

    worked on integrating handling lines and schema

--- python/espctl/__init__.py
+++ python/espctl/__init__.py
@@ -3,3 +3,66 @@ import time
 from contextlib import contextmanager
 from . import protocol
 from .connection import Connection
+
+
+
+_INT_ATTRS={
+    "_conn",
+    "_widgets",
+    "_values",
+    "_schema_event",
+    "_recording",
+    "_record_rows",
+    "_plot_buffers"
+}
+
+
+class ESPCtl:
+    def __init__(self, port=None, timeout=5):
+        self._conn = Connection(port=port)
+        self._widgets = {}
+        self._values = {}
+        self._recording = False
+        self._record_rows = None
+        self._plot_buffers = {}
+        self._request_schema(timeout=timeout)
+
+    def _request_schema(self, timeout=5):
+        self._conn.write_line(protocol.REQUEST_SCHEMA)
+        d = time.time() + timeout
+        while time.time()< d:
+            l = self._conn.readline(timeout=d - time.time())
+            if l is None:
+                break
+            self._handle_line(l)
+            if self.widgets:
+                return
+        raise TimeoutError("NO SCHEMA")
+    def _handle_line(self, line):
+        s = protocol.parseSch(line)
+        if s is not None:
+            self._widgets = {w["id"]: w for w in s.get("widgets", [])}
+            for wid, w in self._widgets.items():
+                if "value" in w:
+                    self._values[wid] = w["value"]
+            return
+        data = protocol.parseData(line)
+        if data is not None:
+            wid, value = data
+            self._values[wid] = value
+            if wid in self._plot_buffers:
+                self._plot_buffers[wid].append((time.time(), value))
+            if self._recording:
+                self._record_rows.append((time.time(), wid, value))
+            return
+        
+
+
+    def poll(self, timeout=0):
+        while True:
+            l = self._conn.readline(timeout=timeout)
+            if l is None:
+                return
+            self._handle_line(l)
+
+    
