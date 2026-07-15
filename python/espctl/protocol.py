cfb3ec7f95a2f00bf81ed8f51cbe69e1c4decfa9
Anirudh Sahu <renudilip19@gmail.com>
2026-07-15 14:30:56 +0530

    added a parser protocol

---
+++ python/espctl/protocol.py
@@ -0,0 +1,40 @@
+import json
+
+def parseSch(line):
+    if not line.startswith("$S"):
+        return None
+    return json.loads(line[2:])
+
+def parseData(line):
+    if not line.startswith("$D"):
+        return None
+    body = line[2:]
+    c = body.rfind(",")
+    if c == -1:
+        return None
+    wid = body[:c]
+    value = float(body[c + 1:])
+    return wid, value
+
+
+def parsePin(line):
+    if not line.startswith("$R"):
+        return None
+    body = line[2:]
+    c = body.rfind(",")
+    if c == -1:
+        return None
+    pin = int(body[:c])
+    state = int(body[c+1:])
+    return pin, state
+
+
+def ispass(line):
+    return not line.startswith("$")
+
+def formatSet(id, val):
+    return "!{},{}".format(id, val)
+def formatPinW(pin,state):
+    return ">{},{}".format(pin, 1 if state else 0)
+def formatPinR(pin):
+    return "<{}".format(pin)
