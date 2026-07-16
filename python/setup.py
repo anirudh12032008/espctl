0100d9040ea252013ce2eccc6f54164ed5546c22
Anirudh Sahu <renudilip19@gmail.com>
2026-07-16 11:17:49 +0530

    added readme in pypi desc

--- python/setup.py
+++ python/setup.py
@@ -1,2 +1,4 @@
 from setuptools import setup, find_packages
-setup(name="espctl", version="0.1.0", description="Control and mointor an ESP32 over serial", packages=find_packages(), install_requires=["pyserial", "pandas", "matplotlib"], python_requires=">=3.8" )
+from pathlib import Path
+desc = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
+setup(name="espctl", version="0.1.1", description="Control and mointor an ESP32 over serial", long_description=desc, long_description_content_type="text/markdown", packages=find_packages(), install_requires=["pyserial", "pandas", "matplotlib"], python_requires=">=3.8" )
